1fe422ac2b30e7316100a8916a60b87365916e71
Hussain-7 <hussain2000.rizvi@gmail.com>
2021-05-19 23:56:20 +0500

    done with left section header and search bar ui

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,31 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from "@material-ui/icons";
 import React from "react";
 import "./Sidebar.css";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="sidebar__header"></div>
-      <div className="sidebar__search"></div>
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start a new chat" />
+        </div>
+      </div>
       <div className="sidebar__chats"></div>
     </div>
   );
