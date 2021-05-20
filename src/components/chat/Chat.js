d2816a6fb4d9994e8ab4fe934c32011b95a96d7a
Hussain-7 <hussain2000.rizvi@gmail.com>
2021-05-21 04:20:28 +0500

    Message sending functionality added msgs save db

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -6,33 +6,58 @@ import {
   MoreVert,
   SearchOutlined,
 } from "@material-ui/icons";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import Moment from "react-moment";
+import moment from "moment";
+import { useStateValue } from "../../contextApi/StateProvider";
 import { useParams } from "react-router-dom";
 import db from "../../config/firebase";
+import firebase from "firebase";
 import "./Chat.css";
 const Chat = () => {
+  const messagesEndRef = useRef(null);
+  const [{ user }, dispatch] = useStateValue();
   const [input, setInput] = useState("");
   const [seed, setSeed] = useState("");
   const { roomId } = useParams();
   const [roomName, setRoomName] = useState("");
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     if (roomId) {
       db.collection("rooms")
         .doc(roomId)
         .onSnapshot((snapshot) => setRoomName(snapshot.data().name));
+
+      db.collection("rooms")
+        .doc(roomId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
     }
   }, [roomId]);
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 500));
   }, []);
-  
+
   const sendMessage = (e) => {
     e.preventDefault();
-    console.log(input);
+    db.collection("rooms").doc(roomId).collection("messages").add({
+      message: input,
+      name: user.displayName,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    // scrollToBottom();
     setInput("");
   };
+  // const scrollToBottom = () => {
+  //   console.log("in scroll to bottom");
+  //   console.log(messagesEndRef);
+  //   messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  // };
   return (
     <div className="chat">
       <div className="chat__header">
@@ -54,11 +79,27 @@ const Chat = () => {
         </div>
       </div>
       <div className="chat__body">
-        <p className={`chat__message ${true && "chat__reciever"}`}>
-          <span className="chat__name">Hussain</span>
-          Hey Guys
-          <span className="chat__timestamp">3:52pm</span>
-        </p>
+        {messages.map((message) => (
+          <p
+            className={`chat__message ${
+              message.name === user.displayName && "chat__reciever"
+            }`}
+          >
+            <span className="chat__name">{message.name}</span>
+            {message.message}
+            <span className="chat__timestamp">
+              {/* {new Date(message.timestamp?.toDate()).toUTCString()} */}
+              {new Date(message.timestamp?.toDate())?.toDateString()}
+              {", "}
+              {new Date(message.timestamp?.toDate()).toLocaleString("en-US", {
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true,
+              })}
+            </span>
+          </p>
+        ))}
+        {/* <p style={{ height: "40px" }} ref={messagesEndRef}></p> */}
       </div>
       <div className="chat__footer">
         <InsertEmoticon />
