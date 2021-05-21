6e788d9ca7be138b30c3799c33f59c4b856bc9a0
Hussain Rizvi <55734494+Hussain-7@users.noreply.github.com>
2021-05-21 18:21:59 +0500

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,5 @@
 import firebase from "firebase";
-const firebaseConfig = {
-  apiKey: "AIzaSyCkrIHhjMi2w021Dute9qJWZwTX7MexWAw",
-  authDomain: "whatsapp-clone-c028b.firebaseapp.com",
-  projectId: "whatsapp-clone-c028b",
-  storageBucket: "whatsapp-clone-c028b.appspot.com",
-  messagingSenderId: "701944732479",
-  appId: "1:701944732479:web:d93bd6afd631fe5c0bd18a",
-  measurementId: "G-69GP8CHNM4",
-};
+const firebaseConfig = {};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
